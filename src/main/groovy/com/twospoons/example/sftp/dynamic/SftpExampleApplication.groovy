dea278142dcd76b7f535f4afdcaef1b13d67b7b3
Chris Tosspon <chris.tosspon@objectpartners.com>
2017-12-10 16:08:15 -0600

    Project set up.

---
+++ src/main/groovy/com/twospoons/example/sftp/dynamic/SftpExampleApplication.groovy
@@ -0,0 +1,15 @@
+package com.twospoons.example.sftp.dynamic
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+/**
+ * Created by ctwospoons on 12/10/17.
+ */
+@SpringBootApplication
+class SftpExampleApplication {
+
+    public static void main(String... args){
+        SpringApplication.run(SftpExampleApplication, args)
+    }
+}
